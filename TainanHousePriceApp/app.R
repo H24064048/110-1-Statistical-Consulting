a577429160a7b611ded8d535877736a2bf813996
Geng-Yu, Li <h24064048@gs.ncku.edu.tw>
2021-12-31 20:53:11 +0800

    Re-organization & Data in

--- TainanHousePriceApp/app.R
+++ TainanHousePriceApp/app.R
@@ -10,10 +10,10 @@ library(scales)
 source("source_plot.R", encoding="utf-8")
 
 #setwd(choose.dir())
-new_house <- read.csv('A_Tainan.csv')
-rent_house <- read.csv('C_Tainan.csv')
+new_house <- read.csv('B2016Q1_2021Q3.csv')
+rent_house <- read.csv('C2016Q1_2021Q3.csv')
 rent_house2 <- rent_house %>% filter(總額元 <= 50000)
-# Define UI for application that draws a histogram
+
 ui <- navbarPage(
     
     # Application title
@@ -21,57 +21,50 @@ ui <- navbarPage(
     theme = shinytheme("sandstone"),
     fluid = TRUE,
     
-    # Sidebar with a slider input for number of bins 
+    # Tab 1 
     tabPanel("資料視察",
-             sidebarLayout(
-                 sidebarPanel(
-                     titlePanel("Title Panel here is 中文"),
-                     
-                     sliderInput("bins",
-                                 "Number of bins:",
-                                 min = 1,
-                                 max = 50,
-                                 value = 30),
-                     
-                     fluidRow(column(3,
-                                     # Select which Gender(s) to plot
-                                     checkboxGroupInput(inputId = "GenderFinder",
-                                                        label = "Select Gender(s):",
-                                                        choices = c("Male" = "M", "Female" = "F"),
-                                                        selected = "M"),
-                                     # Select which Division(s) to plot
-                                     checkboxGroupInput(inputId = "DivisionFinder",
-                                                        label = "Select Division(s):",
-                                                        choices = c("DI", "DII", "DIII"),
-                                                        selected = "DI")
-                     ),
-                     column(6, offset = 2,
-                            # Select which Region(s) to plot
-                            checkboxGroupInput(inputId = "RegionFinder",
-                                               label = "行政區",
-                                               choices = c("安平  區" = "安平區", "東區" = "東區", "西區" = "MidWest", "中西區", "West", "南區" = "南區", "安南區", "永康區", "新市區"),
-                                               selected = "NewEngland")
-                     )),
-                     sliderInput("transactiondate", 
-                                 "交易日期",
-                                 min = 1050101, 
-                                 max = 1081231, 
-                                 value = c(1060101, 1061231))
-                 ),
+        sidebarLayout(
+            sidebarPanel(
+                titlePanel("Title Panel here is 中文"),
+                sliderInput("bins", "Number of bins:", min = 1, max = 50, value = 30),
+                fluidRow(
+                    column(3,
+                        # Select which Gender(s) to plot
+                        checkboxGroupInput(inputId = "GenderFinder",
+                                            label = "Select Gender(s):",
+                                            choices = c("Male" = "M", "Female" = "F"),
+                                            selected = "M"),
+                        # Select which Division(s) to plot
+                        checkboxGroupInput(inputId = "BuildingMaterial",
+                                            label = "主要建材:",
+                                            choices = c("鋼骨混凝土造", "鋼骨鋼筋混凝土造", "鋼筋混凝土造", "鋼筋混凝土構造"),
+                                            selected = c("鋼骨混凝土造", "鋼骨鋼筋混凝土造", "鋼筋混凝土造", "鋼筋混凝土構造"))
+                    ),
+                    column(6, offset = 2,
+                        # Select which Region(s) to plot
+                        checkboxGroupInput(inputId = "RegionFinder",
+                                            label = "行政區",
+                                            choices = c("安平區", "新市區", "東區", "西區", "中西區", "南區", "安南區", "永康區"),
+                                            selected = "安平區")
+                    )
+                ),
+                sliderInput("transactionyear", "交易年分", min = 103, max = 110, value = c(106, 110))
+            ),
                  
-                 mainPanel(
-                     # Show a plot of the generated distribution
-                     plotOutput("distPlot"),
-                     # Main Table
-                     DT::dataTableOutput("mytable")
-                 )
-             ),
+            mainPanel(
+                plotOutput("distPlot"),
+                # Main Table
+                DT::dataTableOutput("mytable")
+            )
+        ),
     ),
+    
+    # Tab 2
     tabPanel("個人化推薦",
-             fluidRow(
-                 wellPanel(style = "background-color: #fff; border-color: #2c3e50; height: 720px;",
-                           plotOutput("Rent_density", height = 680))
-             ) 
+        fluidRow(
+            wellPanel(style = "background-color: #fff; border-color: #2c3e50; height: 720px;",
+                        plotOutput("Rent_density", height = 680))
+        ) 
     ),
     tabPanel("關於")
 )
@@ -80,19 +73,19 @@ ui <- navbarPage(
 server <- function(input, output) {
     
     output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
         x    <- faithful[, 2]
         bins <- seq(min(x), max(x), length.out = input$bins + 1)
-        
-        # draw the histogram with the specified number of bins
         hist(x, breaks = bins, col = 'darkgray', border = 'white')
     })
     
-    output$mytable = DT::renderDataTable({
-        new_house[,c("鄉鎮市區", "交易年月日", "總價元", "建物型態", "建物現況格局.廳", "建物現況格局.衛", "建物現況格局.隔間")] %>%
-            filter(鄉鎮市區 == input$'RegionFinder')%>%
-            filter(交易年月日 > input$'transactiondate'[1] & 交易年月日 < input$'transactiondate'[2])
-    })
+    output$mytable = DT::renderDT({ DT::datatable(
+        new_house[,c("鄉鎮市區", "交易年月日", "總價元", "建物型態", "建物現況格局.房", "建物現況格局.廳", "建物現況格局.衛", "建物型態", "主要建材")] %>%
+            filter(主要建材 %in% input$'BuildingMaterial') %>%
+            filter(鄉鎮市區 %in% input$'RegionFinder') %>%
+            filter((交易年月日 > (input$'transactionyear'[1] *10000)) & (交易年月日 < (((input$'transactionyear'[2]) + 1)*10000))),
+        colnames = c("鄉鎮市區", "交易年月日", "總價元", "建物型態", "房", "廳", "衛", "建物型態", "主要建材"), 
+        escape = FALSE
+    )})
     
     output$Rent_density <- renderPlot({ 
         rentdist_fig(rent_house2)
